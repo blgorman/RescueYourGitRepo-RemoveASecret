ff997b4cbc9a440f262abfaabb19a831ee821376
Brian L. Gorman <blgorman@gmail.com>
2023-04-05 14:18:24 -0500

    Added configurationfile and loaded it

--- SimpleBlobStorageDemo/SimpleBlobStorageDemo/Program.cs
+++ SimpleBlobStorageDemo/SimpleBlobStorageDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 
@@ -7,9 +8,25 @@ namespace SimpleBlobStorageDemo
 {
     public class Program
     {
+        private static IConfigurationRoot _configuration;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            BuildOptions();
+
+            var storageConnection = _configuration["Storage:ConnectionString"];
+            Console.WriteLine($"Connection: {storageConnection}");
+        }
+
+        private static void BuildOptions()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddUserSecrets<Program>()
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            _configuration = builder.Build();
         }
     }
 }
