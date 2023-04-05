543491eee45c07b6a514e31050e315caff56c9ff
Brian L. Gorman <blgorman@gmail.com>
2023-04-05 14:11:46 -0500

    Created the initial project

---
+++ SimpleBlobStorageDemo/SimpleBlobStorageDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+
+namespace SimpleBlobStorageDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
